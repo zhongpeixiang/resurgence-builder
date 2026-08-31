6e9e5a3ef11f9d73b6297f0c3a1f825978304c1d
Zhong Peixiang <zhongpeixiang@gmail.com>
2026-08-31 14:33:44 +0000

    feat: import authorized SHD OS protocols

---
+++ data/os-protocols.js
@@ -0,0 +1,173 @@
+// Generated from an authorized SHD.build OS Protocols-page source. Do not edit by hand.
+export const protocolCatalog = [
+  {
+    "id": "c9703de6-8c13-471a-8729-0969c98dd5f7",
+    "name": "Collateral Damage",
+    "type": "OS Protocol",
+    "core": "Engineering",
+    "description": "While dealing Skill Damage, also deal damage equal to 3000% Engineering to enemies within 5 meters of the target.\r\n\r\nTalent Cooldown: 10 seconds.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "dc7bc24b-739b-41b6-a523-a5a9cb327e09",
+    "name": "Explosive Charge",
+    "type": "OS Protocol",
+    "core": "Engineering",
+    "description": "Shooting for 3 seconds enhances the next Skill for 8 seconds.\r\nDealing damage with the Enhanced Skill creates an explosion with a radius of 5 meters around it that deals damage equal to 1600% Engineering.\r\n\r\nTalent Cooldown: 5 seconds.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "0d1c552d-b55e-4e7a-8645-0cfea7105fba",
+    "name": "Relentless Assault",
+    "type": "OS Protocol",
+    "core": "Engineering",
+    "description": "For every 1 shots fired, grants +{1} Weapon Damage for 5 seconds, up to 30 stacks.\r\nLoses 2 stacks per second.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "5793dfe4-fef7-4210-901d-e8fe78547c8f",
+    "name": "Strategic Assistance",
+    "type": "OS Protocol",
+    "core": "Engineering",
+    "description": "Assists grants +{1} Signature Ability Charge Efficiency for 10 seconds and reduce Skill Cooldown by {2}, up to 5 stacks.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "eaa8a282-499d-44ec-b25d-ba01e90a4bf0",
+    "name": "Surgical Engineering",
+    "type": "OS Protocol",
+    "core": "Engineering",
+    "description": "Skill Critical Hits have a 60% chance of granting +{1} Skill Critical Damage for 10 seconds, up to 3 stacks.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "4ca7b643-e139-40af-baa1-dbb0ad5302e9",
+    "name": "Tactical Layering",
+    "type": "OS Protocol",
+    "core": "Engineering",
+    "description": "Deploying a Skill grants +15% Engineering, up to 3 stacks.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "33cddcd1-daac-4176-8b2f-4de93f9807f9",
+    "name": "Assault Charge",
+    "type": "OS Protocol",
+    "core": "Firepower",
+    "description": "Deploying the Signature Ability grants +{1} Firepower and +{2} Rate of Fire for 20 seconds.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "fe5218b7-00a4-4202-9753-091e7a6f7aa7",
+    "name": "Concerted Strike",
+    "type": "OS Protocol",
+    "core": "Firepower",
+    "description": "Skill Hits within 20 meters have a 100% chance of granting +8% Damage for 10 seconds, up to 3 stacks.\r\n\r\nTalent Cooldown: 1 seconds.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "dadde5e8-dfc4-424a-8da9-c6b5fb241234",
+    "name": "Cover Shooter",
+    "type": "OS Protocol",
+    "core": "Firepower",
+    "description": "Critical Hits behind cover have a 80% chance of granting +16% Weapon Critical Hit Damage for 5 seconds, up to 3 stacks.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "f9e1bea4-1f64-4e98-ab0b-944970ad994c",
+    "name": "Explosive Round",
+    "type": "OS Protocol",
+    "core": "Firepower",
+    "description": "Critical Hits performed during aiming create an explosion with a radius of 5 meters around the target the deals damage equal to 2000% Firepower to them.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "96ab4c8e-51c7-406f-8c04-3508a998dd1e",
+    "name": "Overwhelming Power",
+    "type": "OS Protocol",
+    "core": "Firepower",
+    "description": "Shooting grants +{1}% Weapon Damage for 2 seconds, up to 6 stacks.\r\nShooting for 2 seconds grants +50% Damage reduction for 2 seconds.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "710d2391-9a22-44de-983a-e4d1f1603451",
+    "name": "Rampart",
+    "type": "OS Protocol",
+    "core": "Firepower",
+    "description": "While immobile, grants +{1} Damage for 1 seconds, up to 6 stacks.\r\nThis bonus resets upon moving.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "2d30d956-3bd6-4a3f-b045-f48da11f7ff1",
+    "name": "Happy Survival",
+    "type": "OS Protocol",
+    "core": "Toughness",
+    "description": "Restore 0.50% Max Health for every 1 enemies within 10 meters, up to a maximum of 2.50%.\r\nShooting consumes 2% Health and deals additional damage equal to 0.60% Current Health.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "b168fd75-2d1a-402c-9080-29f41455304c",
+    "name": "Heavy Armor",
+    "type": "OS Protocol",
+    "core": "Toughness",
+    "description": "While possessing Extra Health, grants +14.40% Damage.\r\nAssists grant Extra Health equal to 16% Max Health for 10 seconds.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "b0ac6cb0-0511-45ad-bad4-2452b4b668cb",
+    "name": "Pummeling Shield",
+    "type": "OS Protocol",
+    "core": "Toughness",
+    "description": "While possessing Extra Health, apply Stun after every 10 hits performed.\r\nAt Max Health, grants Extra Health equal to 2400% Firepower for 10 seconds when hit.\r\n\r\nTalent Cooldown: 10 seconds.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "94310942-50da-4deb-8fd8-4fb5124c2223",
+    "name": "Ambusher",
+    "type": "OS Protocol",
+    "core": "Engineering",
+    "description": "Being immobile for 3 seconds grants +{1} Engineering.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  },
+  {
+    "id": "c23f0a1f-fa79-4f24-b354-263cff0582b1",
+    "name": "Close and Personal",
+    "type": "OS Protocol",
+    "core": "Engineering",
+    "description": "Grants +{1}% Engineering for each ally within 30 meters who possesses a buff that was granted by the Agent.",
+    "secondaryDescription": "",
+    "attributes": [],
+    "source": "SHD.build database/os-protocols page provided by authorized contributor"
+  }
+];
